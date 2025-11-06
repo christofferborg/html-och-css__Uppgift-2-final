bcfa9a120f5c206f67ad4d8a8186420109f239ab
Christoffer Borg <christofferborg@me.com>
2025-11-06 13:22:26 +0100

    Reworked menu with JS.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const menuCheckbox = document.querySelector('#menuCheckbox');
+const menuIcon = document.querySelector('.header__menuIcon');
+const mobileMenu = document.querySelector('.header__mobile-menu');
+const closeIcon = document.querySelector('.header__closeIcon');
 
-menuCheckbox.addEventListener('change', () => {
-document.body.classList.toggle('menu-open', menuCheckbox.checked);
+menuIcon.addEventListener('click', () => {
+document.body.classList.add('menu-open');
+mobileMenu.classList.add('is-open');
+closeIcon.classList.add('is-open');
+menuIcon.classList.add('is-open');
+})
+
+closeIcon.addEventListener('click', () => {
+document.body.classList.remove('menu-open');
+mobileMenu.classList.remove('is-open');
+closeIcon.classList.remove('is-open');
+menuIcon.classList.remove('is-open');
 })
